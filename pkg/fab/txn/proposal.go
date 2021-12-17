7e9abac565380ff4a27fff141f81e19750bad640
Andre Staats <wastaats@live.com>
2021-12-17 17:23:10 +0200

    updated lifecycle

--- pkg/fab/txn/proposal.go
+++ pkg/fab/txn/proposal.go
@@ -47,13 +47,13 @@ func CreateChaincodeInvokeProposal(txh fab.TransactionHeader, request fab.Chainc
 		},
 	}
 
-	proposal, txID, err := protoutil.CreateChaincodeProposalWithTxIDAndTransient(common.HeaderType_ENDORSER_TRANSACTION, txh.ChannelID(), ccis, txh.Creator(), string(txh.TransactionID()), request.TransientMap)
+	proposal, _, err := protoutil.CreateChaincodeProposalWithTxIDNonceAndTransient(string(txh.TransactionID()), common.HeaderType_ENDORSER_TRANSACTION, txh.ChannelID(), ccis, txh.Nonce(), txh.Creator(), request.TransientMap)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create chaincode proposal")
 	}
 
 	tp := fab.TransactionProposal{
-		TxnID:    fab.TransactionID(txID),
+		TxnID:    txh.TransactionID(),
 		Proposal: proposal,
 	}
 
